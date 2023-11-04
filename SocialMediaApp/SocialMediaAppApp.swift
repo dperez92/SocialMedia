1e4d5289e8d4f753d97bba4a4b4c50b94195bbb0
Daniel Perez <daniel.perez.olivares@gmail.com>
2023-11-04 11:12:21 -0300

    Initial Commit

---
+++ SocialMediaApp/SocialMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAppApp.swift
+//  SocialMediaApp
+//
+//  Created by Daniel Perez Olivares on 04-11-23.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
